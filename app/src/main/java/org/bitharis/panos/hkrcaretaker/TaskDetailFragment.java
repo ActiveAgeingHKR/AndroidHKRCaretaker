19e2f8163cf06dab9e44aad127baf4819960cb8c
Bitharis <bitharis@gmail.com>
2016-12-22 23:59:14 +0100

    Added Task Detail Fragment (Empty)

---
+++ app/src/main/java/org/bitharis/panos/hkrcaretaker/TaskDetailFragment.java
@@ -0,0 +1,10 @@
+package org.bitharis.panos.hkrcaretaker;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by panos on 12/22/2016.
+ */
+
+public class TaskDetailFragment extends Fragment{
+}
